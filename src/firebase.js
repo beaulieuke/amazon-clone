261da6f3e5001e89cd89671ca0bf75adeb87b358
Kevin Beaulieu <beaulieuke@gmail.com>
2022-01-12 12:30:17 -0500

    firebase update

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,15 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
+  apiKey: "AIzaSyBpTHS6vV51rgUheubViCza-zxcuvvyuHQ",
+  authDomain: "clone-ed309.firebaseapp.com",
+  projectId: "clone-ed309",
+  storageBucket: "clone-ed309.appspot.com",
+  messagingSenderId: "46468449389",
+  appId: "1:46468449389:web:b5def47247bc62e7cdab64",
+  measurementId: "G-PDF526D20Q"
+};
 
-    apiKey: "AIzaSyBpTHS6vV51rgUheubViCza-zxcuvvyuHQ",
-  
-    authDomain: "clone-ed309.firebaseapp.com",
-  
-    projectId: "clone-ed309",
-  
-    storageBucket: "clone-ed309.appspot.com",
-  
-    messagingSenderId: "46468449389",
-  
-    appId: "1:46468449389:web:b5def47247bc62e7cdab64",
-  
-    measurementId: "G-PDF526D20Q"
-
-  };
-  
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 const db = firebaseApp.firestore();
